df185c60c55eb6b72433dee177988ebc357385b8
yoshiya12x <yoppie.bull@gmail.com>
2022-06-16 09:30:11 +0900

    Initial Commit

---
+++ PracticeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticeSwiftUI
+//
+//  Created by 前田　欣耶　 on 2022/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by 前田　欣耶　 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
