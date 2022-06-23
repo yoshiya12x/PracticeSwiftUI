a33c5a44b37eb8b28bce98e6cc6cebde17d0c491
yoshiya12x <yoppie.bull@gmail.com>
2022-06-23 16:35:48 +0900

    typosテスト

---
+++ PracticeSwiftUI/Views/Toy/Atoms/ToyBtton.swift
@@ -0,0 +1,22 @@
+//
+//  ToyBtton.swift
+//  PracticeSwiftUI
+//
+//  Created by 前田　欣耶　 on 2022/06/23.
+//
+
+import SwiftUI
+
+struct ToyBtton: View {
+    var ttle: String
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToyBtton_Previews: PreviewProvider {
+    static var previews: some View {
+        ToyBtton(ttle: "cofee")
+    }
+}
