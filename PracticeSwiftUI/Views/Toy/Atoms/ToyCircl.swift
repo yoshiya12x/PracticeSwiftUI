456122ecfd01456671a700884144f646443d8917
yoshiya12x <yoppie.bull@gmail.com>
2022-06-23 16:44:46 +0900

    typosテスト

---
+++ PracticeSwiftUI/Views/Toy/Atoms/ToyCircl.swift
@@ -0,0 +1,22 @@
+//
+//  ToyCircl.swift
+//  PracticeSwiftUI
+//
+//  Created by 前田　欣耶　 on 2022/06/23.
+//
+
+import SwiftUI
+
+struct ToyCircl: View {
+    var ttle: String
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToyCircl_Previews: PreviewProvider {
+    static var previews: some View {
+        ToyCircl(ttle: "cofee")
+    }
+}
