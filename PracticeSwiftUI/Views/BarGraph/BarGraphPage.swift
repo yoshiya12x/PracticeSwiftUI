fd0f77512d05598cbd1cbe321c9ba235b024ae17
yoshiya12x <yoppie.bull@gmail.com>
2022-07-01 15:58:39 +0900

    BarGraphPageを作成

---
+++ PracticeSwiftUI/Views/BarGraph/BarGraphPage.swift
@@ -0,0 +1,22 @@
+//
+//  BarGraphPage.swift
+//  PracticeSwiftUI
+//
+//  Created by 前田　欣耶　 on 2022/07/01.
+//
+
+import SwiftUI
+
+struct BarGraphPage: View {
+    var body: some View {
+        VStack {
+            Text("棒グラフ")
+        }
+    }
+}
+
+struct BarGraphPage_Previews: PreviewProvider {
+    static var previews: some View {
+        BarGraphPage()
+    }
+}
